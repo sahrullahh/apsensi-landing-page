373b3c10a6c6e68f93df0db387a247c019033ab6
sahrullah <mohammadsahrullah894@gmail.com>
2023-11-28 21:45:40 +0700

    add: hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        "plus-jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
+        "plus-jakarta-sans": "'Plus Jakarta Sans', sans-serif",
+        quicksand: "'Quicksand', sans-serif",
+      },
+      colors: {
+        "primary-text": "#2A3547",
       },
     },
   },
